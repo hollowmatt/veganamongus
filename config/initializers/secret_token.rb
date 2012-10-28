24765ef59fc04fd96ae4b6dc852f8a5629a231f2
Matthew Holloway <matthew.holloway@logicwerx.com>
2012-10-28 19:25:49 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Veganamongus::Application.config.secret_token = 'a7872a2f1da91f50c2e69fb87665ecadb3d5291e16b4430d01612654a565dd2ee4abd25b5589075ff628382dc253d03aef94dda6cf28328bd540469f230c1c36'
